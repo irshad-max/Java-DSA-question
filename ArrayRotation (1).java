e519e8dd76910007c2ad3f693dc89fbab0e7d8d2
irshad-max <khushbudeenshah@gmail.com>
2026-08-15 12:13:50 +0530

    One position Array Rotation(inplace)

---
+++ ArrayRotation (1).java
@@ -0,0 +1,14 @@
+class ArrayRotation{
+    public static void main(String[] args){
+        int arr[]={1,2,3,4,5,6};
+        int n=arr.length;
+        int last=arr[arr.length-1];
+        for(int i=n-1;i>0;i--){
+            arr[i]=arr[i-1];
+        }
+       arr[0]=last;
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
