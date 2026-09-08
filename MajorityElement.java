99883576fff7a6a98a2befcd1b22d353482fe37d
irshad-max <khushbudeenshah@gmail.com>
2026-09-08 22:48:04 +0530

    Majority find under optimization element

---
+++ MajorityElement.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class MajorityElement{
+    public static void main(String[] args) {
+      HashMap<Integer,Integer>list =new HashMap<>();
+      int arr[]={2,2,1,3,2,2,2};
+      int n=arr.length;
+      for(int i=0;i<n;i++){
+          if(list.containsKey(arr[i])){
+             list.put(arr[i],list.getOrDefault(arr[i],0)+1);
+              
+          }
+          else{
+              list.put(arr[i],1);
+          }
+      }
+      for(int key:list.keySet()){
+          if(list.get(key)>n/2){
+           System.out.print(key);
+          }
+      }
+    }
+}
